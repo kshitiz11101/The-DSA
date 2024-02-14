1e315022fb148fbf8926b462c3b4c07e980a8f09
Kshitiz Sharma <110187324+kshitiz11101@users.noreply.github.com>
2024-02-14 18:18:19 +0530

    Create Buy&SellStock.cpp

---
+++ DP/Buy&SellStock.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini=prices[0];
+        int n=prices.size(),profit=0;
+        for(int i=0;i<n;i++){
+            int cost=prices[i]-mini;
+            profit=max(profit,cost);
+            mini=min(mini,prices[i]);
+        }
+        return profit;
+    }
+};
