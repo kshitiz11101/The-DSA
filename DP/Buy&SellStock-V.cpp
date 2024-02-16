ea49e2f22ec4729ac1d6615eeb1f04019347b080
Kshitiz Sharma <110187324+kshitiz11101@users.noreply.github.com>
2024-02-16 18:57:59 +0530

    Update Buy&SellStock-V.cpp

--- DP/Buy&SellStock-V.cpp
+++ DP/Buy&SellStock-V.cpp
@@ -37,3 +37,29 @@ int f(int ind,int buy,vector<int>&prices,int n, vector<vector<int>>&dp){
          return dp[0][0];
     }
 };
+class Solution {
+public:
+
+    int maxProfit(vector<int>& prices) {
+         int n=prices.size();
+        //  vector<vector<int>>dp(n+2,vector<int>(3,0));
+        vector<int>ahead(2,0),curr(2,0),ahead1(2,0);
+        ahead[0]=ahead[1]=0;
+        //  dp[n][0]=dp[n][1]=0;
+         for(int ind=n-1;ind>=0;ind--){
+             for(int buy=0;buy<2;buy++){
+                  int profit=0;
+      if(buy==0){
+          profit=max(-prices[ind]+ahead[1],0+ahead[0]);
+      }
+     if(buy==1){
+          profit=max(prices[ind]+ahead1[0],0+ahead[1]);
+      }
+      curr[buy]=profit;
+             }
+             ahead1=ahead;
+             ahead=curr;
+         }
+         return curr[0];
+    }
+};
