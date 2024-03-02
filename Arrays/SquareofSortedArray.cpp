1c3f29744f78455f0ba8e81b66ca29a5474a7fb9
Kshitiz Sharma <110187324+kshitiz11101@users.noreply.github.com>
2024-03-02 11:27:10 +0530

    Create SquareofSortedArray.cpp

---
+++ Arrays/SquareofSortedArray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            ans.push_back(pow(nums[i],2));
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
