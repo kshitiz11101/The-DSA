c176c1cf3ccd5aa4b6aa7cd19e70485cff8c5226
Kshitiz Sharma <110187324+kshitiz11101@users.noreply.github.com>
2024-03-02 15:41:12 +0530

    Create SingleNumber.cpp

---
+++ Arrays/SingleNumber.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size(),ans=0;
+        for(int i=0;i<n;i++){
+            ans^=nums[i];
+        }
+        return ans;
+    }
+};
