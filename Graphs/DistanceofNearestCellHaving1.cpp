a1c7bf9739da5b54e958967e8e0c0028e8f2939d
Kshitiz Sharma <110187324+kshitiz11101@users.noreply.github.com>
2024-05-23 12:50:47 +0530

    Create DistanceofNearestCellHaving1.cpp

---
+++ Graphs/DistanceofNearestCellHaving1.cpp
@@ -0,0 +1,42 @@
+class Solution 
+{
+    public:
+    //Function to find distance of nearest 1 in the grid for each cell.
+	vector<vector<int>>nearest(vector<vector<int>>grid)
+	{
+	    // Code here
+	    int n=grid.size(),m=grid[0].size();
+	    vector<vector<int>>ans(n,vector<int>(m,0));
+	    vector<vector<int>>visited(n,vector<int>(m,0));
+	    queue<pair<pair<int,int>,int>>q;
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<m;j++){
+	            if(grid[i][j]==1){
+	                q.push({{i,j},0});
+	                visited[i][j]=1;
+	            }
+	            else{
+	                visited[i][j]=0;
+	            }
+	        }
+	    }
+	    int delrow[]={-1,0,+1,0};
+	    int delcol[]={0,+1,0,-1};
+	    while(!q.empty()){
+	        int row=q.front().first.first;
+	        int col=q.front().first.second;
+	        int steps=q.front().second;
+	        q.pop();
+	        ans[row][col]=steps;
+	        for(int i=0;i<4;i++){
+	            int nrow=row+delrow[i];
+	            int ncol=col+delcol[i];
+	            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && visited[nrow][ncol]==0 ){
+	                    visited[nrow][ncol]=1;
+	                    q.push({{nrow,ncol},steps+1});
+	            }
+	        }
+	    }
+	    return ans;
+	}
+};
