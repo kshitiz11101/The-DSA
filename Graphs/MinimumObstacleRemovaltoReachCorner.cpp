b932e1b707de0872581823f819a99ef508005f4b
Kshitiz Sharma <110187324+kshitiz11101@users.noreply.github.com>
2025-01-18 15:17:00 +0530

    Create MinimumObstacleRemovaltoReachCorner.cpp

---
+++ Graphs/MinimumObstacleRemovaltoReachCorner.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minimumObstacles(vector<vector<int>>& grid) {
+        int n=grid.size(),m=grid[0].size();
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
+        pq.push({0,{0,0}});
+        vector<vector<int>> visited(n, vector<int>(m, 1e9));
+        visited[0][0]=0;
+      int delrow[]={1,-1,0,0};  
+    int delcol[]={0,0,-1,1};
+        while(!pq.empty()){
+           int cost=pq.top().first;
+           int row=pq.top().second.first;
+           int col=pq.top().second.second;
+           pq.pop();
+           if(row==n-1 && col==m-1){
+            return cost;
+           }
+
+           for(int ind=0;ind<4;ind++){
+            int nrow=row+delrow[ind];
+            int ncol=col+delcol[ind];
+                       
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
+
+                int ncost=cost+grid[nrow][ncol];
+                if(ncost<visited[nrow][ncol]){
+                visited[nrow][ncol]=ncost;
+                pq.push({ncost,{nrow,ncol}});
+                }
+             
+            
+            }
+           }
+        }
+
+    return 0;
+    }
+};
